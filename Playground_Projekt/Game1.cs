34e2defb5d3f696b1c130d8bbcbfed00cb4f3794
tobiasf <30748375+tobiasfrck@users.noreply.github.com>
2023-04-24 12:38:14 +0200

    updated model, camera movement, camera works

--- Playground_Projekt/Game1.cs
+++ Playground_Projekt/Game1.cs
@@ -22,6 +22,9 @@ namespace Playground_Projekt
         //Orbit
         bool orbit;
 
+        //Content:
+        private Model model;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -43,23 +46,92 @@ namespace Playground_Projekt
             viewMatrix = Matrix.CreateLookAt(camPosition,camTarget,Vector3.Up);
             
             worldMatrix = Matrix.CreateWorld(new Vector3(0f,0f,0f),Vector3.Forward,Vector3.Up);
-
+            
             //BasicEffect
+            basicEffect = new BasicEffect(GraphicsDevice);
+            basicEffect.Alpha = 1.0f;
+
+            // Want to see the colors of the vertices, this needs to be on
+            basicEffect.VertexColorEnabled = true;
+
+            //Lighting requires normal information which VertexPositionColor does not have
+            //If you want to use lighting and VPC you need to create a custom def
+            basicEffect.LightingEnabled = false;
+
+            //Geometry  - a simple triangle about the origin
+            triangleVertices = new VertexPositionColor[3];
+            triangleVertices[0] = new VertexPositionColor(new Vector3(
+                                  0, 20, 0), Color.Red);
+            triangleVertices[1] = new VertexPositionColor(new Vector3(-
+                                  20, -20, 0), Color.Green);
+            triangleVertices[2] = new VertexPositionColor(new Vector3(
+                                  20, -20, 0), Color.Blue);
+
 
         }
 
         protected override void LoadContent()
         {
-            
 
+
+
+            model = Content.Load<Model>("Monkey");
             // TODO: use this.Content to load your game content here
         }
+
+
+
+        private void DrawModel(Model model, Matrix world, Matrix view, Matrix projection)
+        {
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = world;
+                    effect.View = view;
+                    effect.Projection = projection;
+                }
+
+                mesh.Draw();
+            }
+        }
+
+
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                camPosition.X -= 1f;
+                camTarget.X -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                camPosition.X += 1f;
+                camTarget.X += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                camPosition.Y -= 1f;
+                camTarget.Y -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                camPosition.Y += 1f;
+                camTarget.Y += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                camPosition.Z += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.C))
+            {
+                camPosition.Z -= 1f;
+            }
 
+            viewMatrix = Matrix.CreateLookAt(camPosition, camTarget,Vector3.Up);
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -67,10 +139,24 @@ namespace Playground_Projekt
 
         protected override void Draw(GameTime gameTime)
         {
+            basicEffect.Projection = projectionMatrix;
+            basicEffect.View = viewMatrix;
+            basicEffect.World = worldMatrix;
+
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
 
+            RasterizerState rasterizerState = new RasterizerState();
+            rasterizerState.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = rasterizerState;
+
+            foreach (EffectPass pass in basicEffect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                //GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, 3);
+            }
+            // TODO: Add your drawing code here
+            DrawModel(model,worldMatrix,viewMatrix,projectionMatrix);
             base.Draw(gameTime);
         }
     }
