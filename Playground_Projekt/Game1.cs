754ecb52f1fd5f19e5ad7f4e2b1662e5c6ca8c5f
tobiasf <30748375+tobiasfrck@users.noreply.github.com>
2023-04-24 23:29:48 +0200

    almost finished game

--- Playground_Projekt/Game1.cs
+++ Playground_Projekt/Game1.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Timers;
 using System.Transactions;
 
 namespace Playground_Projekt
@@ -18,14 +23,28 @@ namespace Playground_Projekt
 
         BasicEffect basicEffect;
 
-
         //Content:
         public Model ringModel;
-        private Model plane;
-        private Ring[] rings;
+        public Model spaceShip;
+        Vector3 shipPosition;
+        public BoundingBox spaceShipBox;
+        private Ring[] rings2;
+        private List<Ring> rings;
+        private SoundEffect collectSFX;
+        private SoundEffect missedSFX;
+        private SoundEffect winSFX;
+        Texture2D buttonSprite;
+        private SpriteBatch _spriteBatch;
 
         //Gameplay-Variables
         private int score = 0;
+        public int startRingCount = 10;
+        public int distance = 30;
+        public int maxDistance;
+        public Random rnd = new Random();
+        public System.Timers.Timer timer1;
+        public bool allowMovement = true;
+        public ButtonState prevLeftMouseState;
 
         public Game1()
         {
@@ -39,45 +58,68 @@ namespace Playground_Projekt
             // TODO: Add your initialization logic here
             base.Initialize();
 
+            
+            prevLeftMouseState = ButtonState.Released;
+            maxDistance = distance + 50;
+            timer1 = new System.Timers.Timer(30000);
+            timer1.Elapsed += OnTimedEvent;
+            timer1.Start();
+
             //Setup Camera
             camTarget = new Vector3(0f,0f,0f);
-            camPosition = new Vector3(0f,0f,-100f);
+            camPosition = new Vector3(0f,2f,-100f);
 
             projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45),GraphicsDevice.DisplayMode.AspectRatio,1f,1000f);
             
             viewMatrix = Matrix.CreateLookAt(camPosition,camTarget,Vector3.Up);
             
-            worldMatrix = Matrix.CreateWorld(new Vector3(0f,0f,0f),Vector3.Forward,Vector3.Up);
+            //worldMatrix = Matrix.CreateWorld(new Vector3(0f,0f,0f),Vector3.Forward,Vector3.Up);
 
             //Generate Rings
-            Random rnd = new Random();
-            rings = new Ring[rnd.Next(5,10)];
-            int distance = 30;
-            int maxDistance = distance+50;
-            for (int i = 0; i < rings.Length; i++)
+            rings = new List<Ring>();
+            for (int i = 0; i < startRingCount; i++)
             {
                 Vector3 ringPosition = Vector3.Zero;
                 if (i == 0)
                 {
                     ringPosition = new Vector3(rnd.Next(-45, 45), 0, distance);
-                    rings[0] = new Ring(ringPosition, rnd.Next(2, 50), false);
+                    rings.Add(new Ring(ringPosition, rnd.Next(2, 50), false));
                 } else {
-                    ringPosition = new Vector3(rnd.Next(-45, 45), 0, rings[i - 1].getZ() + (rnd.Next(distance, maxDistance)));
-                    rings[i] = new Ring(ringPosition,rnd.Next(2,50),false);
+                    ringPosition = new Vector3(rnd.Next(-45, 45), 0, rings[i-1].getZ() + (rnd.Next(distance, maxDistance)));
+                    rings.Add(new Ring(ringPosition, rnd.Next(2, 50), false));
                 }
                 rings[i].setBoundingBox(calculateBoundingBox(ringModel, Matrix.CreateTranslation(ringPosition)));
             }
-
+            
         }
 
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            timer1.Stop();
+            winSFX.Play(0.25f, 0f, 0f);
+            if (score>600)
+            {
+                Debug.WriteLine("You Won! Points: " + score);
+            }
+            else
+            {
+                Debug.WriteLine("You did not get enough points :( Points: " + score);
+            }
+            timer1.AutoReset=false;
+            allowMovement = false;
+        }
         protected override void LoadContent()
         {
             // TODO: use this.Content to load your game content here
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            buttonSprite = Content.Load<Texture2D>("restartButtonSprite");
             ringModel = Content.Load<Model>("Ring");
-            plane = Content.Load<Model>("plane");
+            spaceShip = Content.Load<Model>("spaceShip");
+            collectSFX = Content.Load<SoundEffect>("beep1");
+            missedSFX = Content.Load<SoundEffect>("woosh");
+            winSFX = Content.Load<SoundEffect>("intro");
         }
-
-
 
         private void DrawModel(Model model, Matrix world, Matrix view, Matrix projection)
         {
@@ -88,8 +130,8 @@ namespace Playground_Projekt
                     effect.World = world;
                     effect.View = view;
                     effect.Projection = projection;
+                    effect.EnableDefaultLighting();
                 }
-                
                 mesh.Draw();
             }
         }
@@ -100,37 +142,34 @@ namespace Playground_Projekt
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            if (Keyboard.GetState().IsKeyDown(Keys.Right) && allowMovement)
             {
                 camPosition.X -= 1f;
                 camTarget.X -= 1f;
             }
-            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            if (Keyboard.GetState().IsKeyDown(Keys.Left) && allowMovement)
             {
                 camPosition.X += 1f;
                 camTarget.X += 1f;
             }
-            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+
+            if (!allowMovement && Mouse.GetState().LeftButton == ButtonState.Pressed && prevLeftMouseState==ButtonState.Released)
             {
-                camPosition.Y += 1f;
-                camTarget.Y += 1f;
+                if (Mouse.GetState().X <= 120 && Mouse.GetState().Y <= 80)
+                {
+                    Debug.WriteLine("Restarted game!");
+                    allowMovement = true;
+                    timer1.Start();
+                    score = 0;
+                }
             }
-            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            prevLeftMouseState = Mouse.GetState().LeftButton;
+
+            if (allowMovement)
             {
-                camPosition.Y -= 1f;
-                camTarget.Y -= 1f;
-            }
-            if (Keyboard.GetState().IsKeyDown(Keys.D))
-            {
-            }
                 camPosition.Z += 1f;
                 camTarget.Z += 1f;
-            if (Keyboard.GetState().IsKeyDown(Keys.C))
-            {
-                camPosition.Z -= 1f;
-                camTarget.Z -= 1f;
             }
-
             if (camPosition.X<-50)
             {
                 camTarget.X = -50;
@@ -142,16 +181,32 @@ namespace Playground_Projekt
                 camPosition.X=50;
             }
             viewMatrix = Matrix.CreateLookAt(camPosition, camTarget,Vector3.Up);
+            shipPosition = Vector3.Add(camPosition, new Vector3(0f, -2f, 10f));
+            spaceShipBox = calculateBoundingBox(spaceShip, Matrix.CreateTranslation(shipPosition));
             // TODO: Add your update logic here
-            Debug.WriteLine(score);
+            //Debug.WriteLine(score);
 
             //PointsManager
-            for(int i = 0; i < rings.Length; i++)
+            for(int i = 0; i < rings.Count; i++)
             {
-                if (rings[i].IsColliding(camPosition))
+                Vector3 ringPosition = Vector3.Zero;
+                if (rings.ElementAt(i).IsColliding(shipPosition))
                 {
-                    score += rings[i].points;
-                    rings[i].dead = true;
+                    score += rings.ElementAt(i).points;
+                    rings.ElementAt(i).dead = true;
+                    rings.RemoveAt(i);
+                    collectSFX.Play(0.25f,0f,0f);
+                    ringPosition = new Vector3(rnd.Next(-45, 45), 0, rings[rings.Count - 1].getZ() + (rnd.Next(distance, maxDistance)));
+                    rings.Add(new Ring(ringPosition, rnd.Next(2, 50), false));
+                    rings[rings.Count - 1].setBoundingBox(calculateBoundingBox(ringModel, Matrix.CreateTranslation(ringPosition)));
+                }
+                else if (camPosition.Z-10f>= rings.ElementAt(i).position.Z) {
+                    rings.ElementAt(i).dead=true;
+                    rings.RemoveAt(i);
+                    missedSFX.Play(0.25f, 0f, 0f);
+                    ringPosition = new Vector3(rnd.Next(-45, 45), 0, rings[rings.Count - 1].getZ() + (rnd.Next(distance, maxDistance)));
+                    rings.Add(new Ring(ringPosition, rnd.Next(2, 50), false));
+                    rings[rings.Count - 1].setBoundingBox(calculateBoundingBox(ringModel, Matrix.CreateTranslation(ringPosition)));
                 }
             }
 
@@ -166,15 +221,26 @@ namespace Playground_Projekt
             rasterizerState.CullMode = CullMode.None;
             GraphicsDevice.RasterizerState = rasterizerState;
 
-
-            // TODO: Add your drawing code here
-            for(int i = 0; i < rings.Length; i++)
+            if(!allowMovement)
             {
-                DrawModel(ringModel, Matrix.CreateTranslation(rings[i].position), viewMatrix,projectionMatrix);
+                _spriteBatch.Begin();
+                _spriteBatch.Draw(buttonSprite, new Rectangle(0, 0,120,80), Color.White);
+                _spriteBatch.End();
             }
 
-            //DrawModel(plane, worldMatrix, viewMatrix, projectionMatrix);
+            GraphicsDevice.DepthStencilState = DepthStencilState.Default; //This fixed broken Models with SpriteBatch and 3D Models
 
+            // TODO: Add your drawing code here
+            for (int i = 0; i < rings.Count; i++)
+            {
+                if (rings.ElementAt(i).dead)
+                {
+                    continue;
+                }
+                DrawModel(ringModel, Matrix.CreateTranslation(rings.ElementAt(i).position), viewMatrix,projectionMatrix);
+            }
+            
+            DrawModel(spaceShip,/*Matrix.CreateRotationX(MathHelper.ToRadians(camPosition.Z))**/Matrix.CreateTranslation(shipPosition), viewMatrix,projectionMatrix);
             base.Draw(gameTime);
         }
         public BoundingBox calculateBoundingBox(Model model, Matrix worldTransform)
